4232251f510da5de7f284c1de374e1b6858c4286
Arefin Anwar <arefinanwar112@gmail.com>
2021-12-01 13:42:11 +0600

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [data, setData] = React.useState([]);
+  React.useEffect(() => {
+    const fetchLocation = async () => {
+      await fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data);
+          console.log(data);
+        });
+    };
+    fetchLocation();
+  }, []);
+  
+  if(!data.length) return <div>Loading...</div>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Name: {data[0].name}
+      <br />
+      Email: {data[0].email}
+      <br />
+      Phone: {data[0].phone}
+      <br/>
+      Address: {data[0].address.street}
     </div>
   );
 }
